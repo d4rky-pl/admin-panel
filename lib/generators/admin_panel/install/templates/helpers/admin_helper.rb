14c159049c86193c51a31fdc730cfa4d12329f35
Michał Matyas <github@higher.lv>
2014-07-08 15:37:19 +0200

    Add simple_form_for helper. Fixes #6

--- lib/generators/admin_panel/install/templates/helpers/admin_helper.rb
+++ lib/generators/admin_panel/install/templates/helpers/admin_helper.rb
@@ -20,4 +20,20 @@ module AdminHelper
   def per_page_class(klass, i)
     (params[:per_page].to_i == i || (params[:per_page].nil? && klass.default_per_page == i)) ? 'btn-primary' : 'btn-default'
   end
+
+  def simple_bootstrap_form_for(resource, opts={}, &block)
+    type = opts.delete(:type) || :horizontal
+    raise 'Supported types: horizontal, vertical' unless type.to_sym.in? [ :horizontal, :vertical ]
+
+    opts = {
+        html: { class: "form-#{type}" },
+        wrapper: :"#{type}_form",
+        wrapper_mappings: { check_boxes: :"#{type}_radio_and_checkboxes",
+                            radio_buttons: :"#{type}_radio_and_checkboxes",
+                            file: :"#{type}_file_input",
+                            boolean: :"#{type}_boolean" }
+    }.merge!(opts)
+
+    simple_form_for(resource, opts, &block)
+  end
 end
